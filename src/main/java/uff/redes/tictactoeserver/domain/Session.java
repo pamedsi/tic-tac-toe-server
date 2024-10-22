7e7eb74873a63e2c233869b07aea8b630f6721d5
Patrick Melo <patrickabimael@yahoo.com.br>
2024-10-22 08:20:28 -0300

    ♻️ refactor: Usa classe invés de record para session

--- src/main/java/uff/redes/tictactoeserver/domain/Session.java
+++ src/main/java/uff/redes/tictactoeserver/domain/Session.java
@@ -4,7 +4,30 @@ import uff.redes.tictactoeserver.dto.Player;
 
 import java.util.UUID;
 
-public record Session (
-        UUID id,
-        Player player
-) {}
+public class Session {
+    private final UUID id;
+    private final Player player;
+    private boolean WSConnected;
+
+    public Session(UUID id, Player player){
+        this.id = id;
+        this.player = player;
+        this.WSConnected = false;
+    }
+
+    public UUID getID() {
+        return id;
+    }
+
+    public Player getPlayer () {
+        return player;
+    }
+
+    public boolean isWSConnected() {
+        return WSConnected;
+    }
+
+    public void setWSConnected(boolean WSConnected) {
+        this.WSConnected = WSConnected;
+    }
+}
