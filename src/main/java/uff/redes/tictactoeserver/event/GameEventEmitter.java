64f4953d9fc06fa7ed4856ae35660710bd3ccdca
Patrick Melo <patrickabimael@yahoo.com.br>
2024-10-22 08:14:05 -0300

    🚚 chore: Renomeia objeto de evento do jogo

--- src/main/java/uff/redes/tictactoeserver/event/GameEventEmitter.java
+++ src/main/java/uff/redes/tictactoeserver/event/GameEventEmitter.java
@@ -2,7 +2,7 @@ package uff.redes.tictactoeserver.event;
 
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
-import uff.redes.tictactoeserver.domain.GameEvent;
+import uff.redes.tictactoeserver.dto.GameEventDTO;
 
 @Service
 public class GameEventEmitter {
@@ -12,7 +12,7 @@ public class GameEventEmitter {
         this.messagingTemplate = messagingTemplate;
     }
 
-    public void emmit(GameEvent event) {
+    public void emmit(GameEventDTO event) {
         messagingTemplate.convertAndSend("/topic/game", event);
     }
 }
