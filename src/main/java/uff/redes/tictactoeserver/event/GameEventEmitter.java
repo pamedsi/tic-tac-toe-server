3f34bcd4137d9b16f6aaa38e7a685f8cb1b908c2
Patrick Melo <patrickabimael@yahoo.com.br>
2024-10-27 20:40:09 -0300

    ♻️ refactor: Remove thread que espera alguns segundos para enviar mensagem

--- src/main/java/uff/redes/tictactoeserver/event/GameEventEmitter.java
+++ src/main/java/uff/redes/tictactoeserver/event/GameEventEmitter.java
@@ -2,12 +2,7 @@ package uff.redes.tictactoeserver.event;
 
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
-import uff.redes.tictactoeserver.domain.GameEvent;
 import uff.redes.tictactoeserver.dto.GameEventDTO;
-
-import java.util.concurrent.CompletableFuture;
-
-import static java.lang.Thread.sleep;
 
 @Service
 public class GameEventEmitter {
@@ -18,20 +13,7 @@ public class GameEventEmitter {
     }
 
     public void emmit(GameEventDTO event) {
-        if (event.type() == GameEvent.FIRST_PLAYER_JOINED || event.type() == GameEvent.BOTH_PLAYERS_JOINED) {
-            CompletableFuture.runAsync(() -> {
-                try {
-                    sleep(1000);
-                    messagingTemplate.convertAndSend("/topic/game", event);
-                }
-                catch (InterruptedException exception) {
-                    System.out.println("Interrupted while waiting for event" + exception.getMessage());
-                }
-            });
-        }
-        else {
             messagingTemplate.convertAndSend("/topic/game", event);
         }
-    }
 
 }
