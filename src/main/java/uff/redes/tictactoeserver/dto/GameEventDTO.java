8d5363d1fa96973ea1fc8c14e564af3462ab6400
Patrick Melo <patrickabimael@yahoo.com.br>
2024-10-27 20:39:44 -0300

    ♻️ refactor: Implementa mais um construtor para não precisar passar valores nulos

--- src/main/java/uff/redes/tictactoeserver/dto/GameEventDTO.java
+++ src/main/java/uff/redes/tictactoeserver/dto/GameEventDTO.java
@@ -10,4 +10,8 @@ public record GameEventDTO(
         GameEvent type,
         Optional<UserDTO> player,
         Optional<MoveRequest> move
-) {}
+) {
+    public GameEventDTO (GameEvent type) {
+        this(type, Optional.empty(), Optional.empty());
+    }
+}
