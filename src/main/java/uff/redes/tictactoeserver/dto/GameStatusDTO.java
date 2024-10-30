7205261bc163b57c57a6b0bd81e78b2a28df9da9
Patrick Melo <patrickabimael@yahoo.com.br>
2024-10-30 09:15:03 -0300

    ♻️ refactor: Passa o grid na busca pelo status

--- src/main/java/uff/redes/tictactoeserver/dto/GameStatusDTO.java
+++ src/main/java/uff/redes/tictactoeserver/dto/GameStatusDTO.java
@@ -1,7 +1,11 @@
 package uff.redes.tictactoeserver.dto;
 
+import uff.redes.tictactoeserver.domain.Cell;
 import uff.redes.tictactoeserver.domain.GameStatus;
 
+import java.util.List;
+
 public record GameStatusDTO(
-        GameStatus status
+        GameStatus status,
+        List<List<Cell>> grid
 ) {}
