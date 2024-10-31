e4e4464c36d300fe807415f568f120f05bfeb739
Patrick Melo <patrickabimael@yahoo.com.br>
2024-10-31 07:37:13 -0300

    ✨️ feat: Checa se a partida terminou

--- src/main/java/uff/redes/tictactoeserver/service/GameService.java
+++ src/main/java/uff/redes/tictactoeserver/service/GameService.java
@@ -5,25 +5,24 @@ import org.springframework.stereotype.Service;
 import uff.redes.tictactoeserver.domain.Cell;
 import uff.redes.tictactoeserver.domain.GameEvent;
 import uff.redes.tictactoeserver.domain.GameStatus;
+import uff.redes.tictactoeserver.domain.Score;
 import uff.redes.tictactoeserver.dto.*;
 import uff.redes.tictactoeserver.event.GameEventEmitter;
 import uff.redes.tictactoeserver.exception.ServerException;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class GameService {
     private final GameEventEmitter gameEventEmitter;
+    private Score score;
     private GameStatus gameStatus;
     private List<List<Cell>> grid;
     private int moves;
 
     public GameService(GameEventEmitter gameEventEmitter){
-        this.initGrid();
-        this.gameStatus = GameStatus.WAITING_FIRST_PLAYER;
-        this.moves = 0;
+        initGame();
         this.gameEventEmitter = gameEventEmitter;
     }
 
@@ -32,30 +31,36 @@ public class GameService {
             throw new ServerException("Game already started!", HttpStatus.CONFLICT);
         }
         gameStatus = GameStatus.X_TURN;
-        printGrid();
+        initGrid();
+        moves = 0;
         gameEventEmitter.emmit(new GameEventDTO(GameEvent.MATCH_STARTED));
-    }
-
-    public void printGrid() {
-        System.out.println(grid.get(0));
-        System.out.println(grid.get(1));
-        System.out.println(grid.get(2));
     }
 
     public void move(MoveRequest moveRequest, Player player) {
         validateMove(moveRequest, player);
-        List<Cell> row = grid.get(moveRequest.row());
-        row.set(moveRequest.column(), Cell.valueOf(player.toString()));
-        printGrid();
+        executeMove(moveRequest.row(), moveRequest.column(), Cell.valueOf(player.toString()));
+        this.moves++;
+        if (!matchIsOver()) {
+            switchTurn();
+            emmitMove(moveRequest, player);
+        }
+    }
+
+    private void emmitMove(MoveRequest moveRequest, Player player) {
+        gameEventEmitter.emmit(new GameEventDTO(
+                GameEvent.MOVE,
+                new UserDTO(false, player),
+                new MoveEvent(player, moveRequest.row(), moveRequest.column())
+        ));
+    }
+
+    private void switchTurn() {
         if (gameStatus == GameStatus.X_TURN) gameStatus = GameStatus.O_TURN;
         else gameStatus = GameStatus.X_TURN;
-        this.moves++;
-        GameEventDTO gameEventDTO = new GameEventDTO (
-                GameEvent.MOVE,
-                Optional.of(new UserDTO(false, player)),
-                Optional.of(new MoveEvent(player, moveRequest.row(), moveRequest.column()))
-        );
-        gameEventEmitter.emmit(gameEventDTO);
+    }
+
+    private void executeMove(int rowIndex, int columnIndex, Cell value) {
+        grid.get(rowIndex).set(columnIndex, value);
     }
 
     private void initGrid() {
@@ -88,19 +93,21 @@ public class GameService {
         this.gameStatus = gameStatus;
     }
 
-    public void restart() {
+    public void initGame() {
         initGrid();
-        this.gameStatus = GameStatus.WAITING_FIRST_PLAYER;
+        gameStatus = GameStatus.WAITING_FIRST_PLAYER;
+        score = new Score();
+        moves = 0;
     }
 
-    private void checkWinner() {
-
+    private boolean thereIsAWinner() {
+        return IsThereAWinnerHorizontally() || IsThereAWinnerVertically() || IsThereAWinnerDiagonally();
     }
 
     private boolean IsThereAWinnerHorizontally() {
         boolean hasAWinnerHorizontally = false;
         for (List<Cell> row : this.grid) {
-            if (row.stream().distinct().count() == 1) {
+            if (row.stream().distinct().count() == 1 && row.getFirst() != Cell.EMPTY) {
                 hasAWinnerHorizontally = true;
                 break;
             }
@@ -108,15 +115,62 @@ public class GameService {
         return hasAWinnerHorizontally;
     }
 
-    private boolean checkIfThereIsAWinnerVertically() {
+    private boolean IsThereAWinnerVertically() {
         for (int columIndex = 0; columIndex < 3; columIndex++) {
             List<Cell> column = List.of(
                     this.grid.get(0).get(columIndex),
                     this.grid.get(1).get(columIndex),
                     this.grid.get(2).get(columIndex)
             );
-            if (column.stream().distinct().count() == 1) return true;
+            if (column.stream().distinct().count() == 1 && column.getFirst() != Cell.EMPTY) return true;
         }
         return false;
     }
+
+    private boolean IsThereAWinnerDiagonally() {
+        Cell topLeftValue = this.grid.get(0).getFirst();
+        boolean allEqualFromTopLeftToBottomRight = topLeftValue == this.grid.get(1).get(1) && topLeftValue == this.grid.get(2).get(2);
+        if (allEqualFromTopLeftToBottomRight && topLeftValue != Cell.EMPTY) return true;
+
+        Cell topRightValue = this.grid.get(0).get(2);
+        boolean allEqualFromTopRightToBottomLeft = topRightValue == this.grid.get(1).get(1) && topRightValue == this.grid.get(2).getFirst();
+        return allEqualFromTopRightToBottomLeft && topRightValue != Cell.EMPTY;
+    }
+
+    private boolean itsADrawGame() {
+        return moves == 9 && (gameStatus == GameStatus.X_TURN || gameStatus == GameStatus.O_TURN);
+    }
+
+    private boolean matchIsOver() {
+        if (moves < 5) return false;
+        Cell winner = null;
+        boolean hasAWinner = false;
+        if (thereIsAWinner()) {
+            winner = gameStatus == GameStatus.X_TURN ? Cell.X : Cell.O;
+            hasAWinner = true;
+        }
+        else if (itsADrawGame()) {
+            winner = Cell.EMPTY;
+            hasAWinner = true;
+        }
+        if (hasAWinner) {
+            score(winner);
+            return true;
+        }
+        return false;
+    }
+
+    private void score(Cell winner) {
+        switch (winner) {
+            case EMPTY -> score.addDraw();
+            case X -> score.addXScore();
+            case O -> score.addOScore();
+        }
+        gameEventEmitter.emmit(new GameEventDTO(
+                GameEvent.MATCH_ENDED,
+                winner
+        ));
+        gameStatus = GameStatus.WAITING_START;
+    }
+
 }
