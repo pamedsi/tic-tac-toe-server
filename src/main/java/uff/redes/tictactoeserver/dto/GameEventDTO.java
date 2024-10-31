69d859d460d7644687289b686829d0b3828affa7
Patrick Melo <patrickabimael@yahoo.com.br>
2024-10-31 07:36:37 -0300

    ✨️ feat: Acrescenta construtores evento de movimento e de fim de partida

--- src/main/java/uff/redes/tictactoeserver/dto/GameEventDTO.java
+++ src/main/java/uff/redes/tictactoeserver/dto/GameEventDTO.java
@@ -1,6 +1,7 @@
 package uff.redes.tictactoeserver.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import uff.redes.tictactoeserver.domain.Cell;
 import uff.redes.tictactoeserver.domain.GameEvent;
 
 import java.util.Optional;
@@ -8,10 +9,19 @@ import java.util.Optional;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record GameEventDTO(
         GameEvent type,
-        Optional<UserDTO> player,
+        Optional<Cell> winner,
+        Optional<UserDTO> user,
         Optional<MoveEvent> move
 ) {
     public GameEventDTO (GameEvent type) {
-        this(type, Optional.empty(), Optional.empty());
+        this(type, Optional.empty(), Optional.empty(), Optional.empty());
+    }
+
+    public GameEventDTO(GameEvent gameEvent, UserDTO userDTO, MoveEvent moveEvent) {
+        this(gameEvent, Optional.empty(), Optional.of(userDTO), Optional.of(moveEvent));
+    }
+
+    public GameEventDTO(GameEvent gameEvent, Cell cell) {
+        this(gameEvent, Optional.of(cell), Optional.empty(), Optional.empty());
     }
 }
